f45dde578cd3d320884b36faf6e2e6b4785ab356
Samad ubuntu-vm <samadarshad@github.com>
2021-01-07 15:45:37 +0000

    file-loader with cat.png

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,7 @@
+import { handleSubmit } from './js/formHandler'
+import { checkForName } from './js/nameChecker'
+import img from '../../cat.png';
+
+console.log(checkForName);
+
 alert("I EXIST")
--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,17 @@ const webpack = require('webpack')
 
 module.exports = {
     entry: './src/client/index.js',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: "file-loader"
+            }
+        ]
+    }
 }
